e17889f16165cc9a110779acc0eaf4c6fe23d50c
Koichi Shiraishi <zchee.io@gmail.com>
2025-04-06 12:44:11 +0900

    metrics: change get_metrics to query_metrics
    "get_metrics" called not Get metric metadata (api/v1/metrics/{metric_name}).
    Changed to correspond to the official API Reference.
    
    ref: https://docs.datadoghq.com/api/latest/metrics/#query-timeseries-points
    
    Signed-off-by: Koichi Shiraishi <zchee.io@gmail.com>

--- src/tools/metrics/schema.ts
+++ src/tools/metrics/schema.ts
@@ -1,11 +1,17 @@
 import { z } from 'zod'
 
-export const GetMetricsZodSchema = z.object({
-  from: z.number().describe('Start time in epoch seconds'),
-  to: z.number().describe('End time in epoch seconds'),
+export const QueryMetricsZodSchema = z.object({
+  from: z
+    .number()
+    .describe(
+      'Start of the queried time period, seconds since the Unix epoch.',
+    ),
+  to: z
+    .number()
+    .describe('End of the queried time period, seconds since the Unix epoch.'),
   query: z
     .string()
     .describe('Datadog metrics query string. e.g. "avg:system.cpu.user{*}'),
 })
 
-export type GetMetricsArgs = z.infer<typeof GetMetricsZodSchema>
+export type QueryMetricsArgs = z.infer<typeof QueryMetricsZodSchema>
